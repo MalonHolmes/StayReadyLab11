0fdf74dc1b20e5966bec62bab9ad94a3fd123c4a
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-24 00:22:12 -0400

    Part1 done

--- src/main/java/SpellChecker.java
+++ src/main/java/SpellChecker.java
@@ -1,2 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class SpellChecker {
+
+    Set<String> reference;
+
+    public String parse(File f){
+        // parse all the text into the HashSet
+        try {
+            reference = new HashSet<String>();  // initialize a new hashset
+            Scanner fileRead = new Scanner(f);
+            while (fileRead.hasNext()) { // iter thru the file
+                String currWord = fileRead.next().replaceAll("[^a-zA-Z0-9]", ""); // removes all punctuation and crap from the current word
+//                if (!reference.contains(currWord.toLowerCase())) {    // if reference doesnt contain currWord, then add it to the set
+//                    reference.add(currWord.toLowerCase());
+//                }
+                reference.add(currWord.toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return reference.toString();
+    }
+
+//    public String getSet(){
+//        Object[] arr = reference.toArray();
+//        String setValues = "";
+//        for (int i = 0; i < arr.length; i++){
+//            setValues += arr[i];
+//            if(i < arr.length - 1){
+//                setValues = setValues + ", ";
+//            }
+//        }
+//        return "[" + setValues + "]";
+//    }
+
+    public static void main(String[] args) {
+        File f = new File("letters_from_gandhi_v2.txt");
+        File g = new File("words_alpha.txt");
+        SpellChecker sc = new SpellChecker();
+        System.out.println(sc.parse(g));
+    }
+
+    // words_alpha.txt works
+    // letter_from_gandhi.txt has the wrong input stream apparently
 }
--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -1,2 +1,30 @@
-public class SpellCheckerTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class SpellCheckerTest{
+
+
+    @Test
+    public void parseTest(){
+        // Given
+        File f = new File("letters_from_gandhi_v2.txt");
+        SpellChecker sc = new SpellChecker();
+
+        // When
+        String expected = "[, but, erred, been, reqwest, paye, your, listen, without, sinceire, impertinance, " +
+                "that, humenity, would, price, shunned, friend, me, from, because, state, has, wor, sake, if, you, which, reduce, " +
+                "considreable, soomething, method, in, is, feeling, it, my, wryte, an, friends, whatever, uging, success, letter, apppeal," +
+                " must, remein, qwuite, who, persan, however, be, for, their, worthy, worth, prevent, can, not, world, are, and, of, today, " +
+                "obejct, yur, writing, have, dear, gandhi, make, a, may, deliberately, will, one, clear, i, war, appeal, k, any, m, tto," +
+                " savoge, way, the, tells, appear, caclulate, forgivness, anticipate, to, resissted]";
+        String actual = sc.parse(f);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
